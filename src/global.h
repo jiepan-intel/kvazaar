47f015b7b238f1c1fa59700bd4c291ce4103f290
jiepan <jie.pan@intel.com>
2025-01-16 11:09:35 +0800

    wasm avx build

--- src/global.h
+++ src/global.h
@@ -310,8 +310,9 @@ typedef enum { COLOR_Y = 0, COLOR_U, COLOR_V } color_t;
 
 
 // Hardware data (abstraction of defines). Extend for other compilers
-#if defined(_M_IX86) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(_M_X64) || defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
+#if defined(_M_IX86) || defined(__i386__) || defined(__i486__) || defined(__i586__) || defined(__i686__) || defined(_M_X64) || defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__) || defined(__EMSCRIPTEN__)
 #  define COMPILE_INTEL 1
+#define __x86_64__
 #else
 #  define COMPILE_INTEL 0
 #endif
